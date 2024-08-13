a1563a6309bae8fa6d01b5efea500ca485fb2583
Junior_Sam <samjunior629@gmail.com>
2024-08-13 06:38:13 +0000

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,62 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(GreetingApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class GreetingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Greeting App'),
+        ),
+        body: GreetingScreen(),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class GreetingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Hello, World!',
+            style: TextStyle(
+              color: Colors.red, // Red color
+              fontWeight: FontWeight.bold, // Bold text
+              fontSize: 24.0,
+            ),
+          ),
+          Text(
+            'Welcome to Flutter!',
+            style: TextStyle(
+              fontSize: 18.0,
+            ),
+          ),
+          // Add an image below the new text
+          Image.network(
+              'https://picsum.photos/250?image=9'), // Optional: Replace with your image URL or use an asset image.
+          SizedBox(
+              height: 20), // Adds some space between the image and the button
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.green, // Green button color
+            ),
+            onPressed: () {
+              // Show a Snackbar when the button is pressed
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Button Pressed!')),
+              );
+            },
+            child: Text('Press Me'),
+          ),
+        ],
       ),
     );
   }
